0903598962b5d0b786c74cfe7da79ec5edc0db91
Izaias Nascimento <izaias.o.nascimento@gmail.com>
2023-12-29 01:34:22 -0300

    Feature: url typebot parametrizado nas configurações da empresa empresa

--- backend/src/services/TypebotService/apiTypebotService.ts
+++ backend/src/services/TypebotService/apiTypebotService.ts
@@ -2,8 +2,8 @@ import AppError from "../../errors/AppError";
 import axios, { AxiosInstance } from "axios";
 import Setting from "../../models/Setting";
 
-const URLTYPEBOTBUILDER = process.env.TYPEBOT_BUILDER_URL
-const URLTYPEBOTVIEWER = process.env.TYPEBOT_VIEWER_URL
+// const URLTYPEBOTBUILDER = process.env.TYPEBOT_BUILDER_URL
+// const URLTYPEBOTVIEWER = process.env.TYPEBOT_VIEWER_URL
 
 // Service Api Typebot version 2.20.0
 
@@ -11,52 +11,83 @@ const apiBuilder = async (companyId): Promise<AxiosInstance> => {
     
   const SettingtokenTypebot = await Setting.findOne({
     where: {
-      key: "tokenTypebot",
       companyId
      }
   });
-  
-  // var tokenTypebot = "UhsQx2ZNHMcCm2u6eOQ2rbt4"
-  var tokenTypebot = SettingtokenTypebot.value
 
+  let urlTypebotBuilder;
+  let tokenTypebot;
+
+  
+  if (Array.isArray(SettingtokenTypebot)) {
+    urlTypebotBuilder = SettingtokenTypebot.find(
+      (d) => d.key === "urlTypebotBuilder"
+    );
+    tokenTypebot = SettingtokenTypebot.find(
+      (d) => d.key === "tokenTypebot"
+    );
+  }
   return axios.create({
-    baseURL: URLTYPEBOTBUILDER,
+    baseURL: urlTypebotBuilder?.value,
     withCredentials: true,
     headers: {
-      Authorization: `Bearer ${tokenTypebot}`,
+      Authorization: `Bearer ${tokenTypebot?.value}`,
       Accept: "application/json"
     }
   })
 }
 
+const apiViewer = async (companyId): Promise<AxiosInstance> => { 
+    
+  const SettingtokenTypebot = await Setting.findOne({
+    where: {
+      companyId
+     }
+  });
+  let urlTypebotViewer;
 
-const apiViewer = axios.create({
-	baseURL: URLTYPEBOTVIEWER,
-  headers: {
-    Accept: "application/json"
+  
+  if (Array.isArray(SettingtokenTypebot)) {
+    urlTypebotViewer = SettingtokenTypebot.find(
+      (d) => d.key === "urlTypebotViewer"
+    );
   }
-});
+  return axios.create({
+    baseURL: urlTypebotViewer?.value,
+    withCredentials: true,
+    headers: {
+      Accept: "application/json"
+    }
+  })
+}
 
-export const startChat = async (msgBody, typebot) => {
+// const apiViewer = axios.create({
+// 	baseURL: urlTypebotViewer?.value,
+//   headers: {
+//     Accept: "application/json"
+//   }
+// });
+
+export const startChat = async (msgBody, typebot,companyId) => {
   const reqData = {
     message: msgBody
   };
-  
+  const api = await apiViewer(companyId)
   try {
-    const request = await apiViewer.post(`${URLTYPEBOTVIEWER}/api/v1/typebots/${typebot}/startChat`, reqData);
+    const request = await api.post(`/api/v1/typebots/${typebot}/startChat`, reqData);
     return request.data
   } catch (error) {
     
   } 
 }
 
-export const continueChat = async (msgBody, sessiontypebot) => {
+export const continueChat = async (msgBody, sessiontypebot, companyId) => {
   const reqData = {
     message: msgBody
   };
-
+  const api = await apiViewer(companyId)
   try {
-    const request = await apiViewer.post(`${URLTYPEBOTVIEWER}/api/v1/sessions/${sessiontypebot}/continueChat`, reqData);
+    const request = await api.post(`/api/v1/sessions/${sessiontypebot}/continueChat`, reqData);
     return request.data
     
   } catch (error) {
@@ -71,7 +102,7 @@ export const listWorkspace = async (companyId) => {
     return request.data
     
   } catch (error) {
-    
+    throw new AppError("ERR_TYPEBOTS_NOT",403)
   } 
 }
 
